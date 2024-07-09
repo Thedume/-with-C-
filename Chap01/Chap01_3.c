c3a12f390a33f6d5f19227eb9dcfb9c2120ff775
진영 조 <jjojin97@gmail.com>
2024-07-09 16:55:46 +0900

    노드 생성, 소멸 구현

--- Chap01/Chap01_3.c
+++ Chap01/Chap01_3.c
@@ -9,7 +9,23 @@ typedef struct tagNode {
 	struct tagNode* NextNode;
 } Node;
 
+// 노드 생성
+Node* DLL_CreateNode(ElementType NewData)
+{
+	Node* NewNode = (Node*)malloc(sizeof(Node));
 
+	NewNode->Data = NewData;
+	NewNode->PrevNode = NULL;
+	NewNode->NextNode = NULL;
+
+	return NewNode;
+}
+
+// 노드 소멸
+void DLL_DestroyNode(Node* Node)
+{
+	free(Node);
+}
 
 int main() {
 
