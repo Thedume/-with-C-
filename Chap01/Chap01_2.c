03f20df1bcfd65397d39ba536ee0ef7317fadf73
진영 조 <jjojin97@gmail.com>
2024-07-09 15:59:14 +0900

    노드 탐색 연산 함수 및 메인 함수에서 테스트

--- Chap01/Chap01_2.c
+++ Chap01/Chap01_2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // 링크드 리스트의 노드 표현
 typedef int ElementType; // int 로 선언 했지만, 나중에 작성할 데이터의 자료형에 따라 변환 가능
 
 typedef struct tagNode {
 	ElementType Data;
-	struct Node* NextNode;
+	struct tagNode* NextNode;
 } Node;
 
 // 노드 생성
@@ -40,16 +41,35 @@ void SLL_AppendNode(Node** Head, Node* NewNode)
 	}
 }
 
+// 노드 탐색 연산
+Node* SLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head;
+
+	while (Current != NULL && (--Location) >= 0)
+		Current = Current->NextNode;
+
+	return Current;
+}
+
+
+
 int main() {
 	// Vitamin QUIZ 1-1
 	// printf("sizeof(Node): %d, sizeof(Node*): %d.\n", sizeof(Node), sizeof(Node*));
 
 	Node* List = NULL;
-	Node* NewNode = NULL;
-	NewNode = SLL_CreateNode(117);	// 자유 저장소에 노드 생성
-	SLL_AppendNode(&List, NewNode); // 생성한 노드를 List에 추가
-	NewNode = SLL_CreateNode(119);	// 자유 저장소에 또 다른 노드 생성
-	SLL_AppendNode(&List, NewNode);	// 생성한 노드를 List에 추가
+	//Node* NewNode = NULL;
+	//NewNode = SLL_CreateNode(117);	// 자유 저장소에 노드 생성
+	//SLL_AppendNode(&List, NewNode); // 생성한 노드를 List에 추가
+	//NewNode = SLL_CreateNode(119);	// 자유 저장소에 또 다른 노드 생성
+	//SLL_AppendNode(&List, NewNode);	// 생성한 노드를 List에 추가
+
+	SLL_AppendNode(&List, SLL_CreateNode(117));
+	SLL_AppendNode(&List, SLL_CreateNode(119));
+
+	Node* myNode = SLL_GetNodeAt(List, 1);	// 두 번째 노드의 주소를 myNode에 저장
+	printf("%d\n", myNode->Data);			// 119를 출력
 
 	return 0;
 }
