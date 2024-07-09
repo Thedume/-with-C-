c891f2098530f48c6ccee6fa637180e475e626e1
진영 조 <jjojin97@gmail.com>
2024-07-09 16:53:40 +0900

    노드 구조체 선언

--- Chap01/Chap01_3.c
+++ Chap01/Chap01_3.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int ElementType;
+
+typedef struct tagNode {
+	ElementType Data;
+	struct tagNode* PrevNode;
+	struct tagNode* NextNode;
+} Node;
+
 
 
 int main() {
