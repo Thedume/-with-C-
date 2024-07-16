ef98d673595ebcaf76442aeb8284408f21ff1fa1
진영 조 <jjojin97@gmail.com>
2024-07-16 15:10:01 +0900

    노드 삭제 연산

--- Chap01/Chap01_3.c
+++ Chap01/Chap01_3.c
@@ -45,6 +45,39 @@ void DLL_AppendNode(Node** Head, Node* NewNode)
 	}
 }
 
+// 노드 탐색
+Node* DLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head;
+
+	while (Current != NULL && (--Location) >= 0)
+		Current = Current->NextNode;
+
+	return Current;
+}
+
+// 노드 삭제
+void DDL_RemoveNode(Node** Head, Node* Remove) 
+{
+	if (*Head == Remove) {
+		*Head = Remove->NextNode;
+		if ((*Head) != NULL)
+			(*Head)->PrevNode = NULL;
+	}
+	else {
+		Node* Temp = Remove;
+
+		if (Remove->PrevNode != NULL)
+			Remove->PrevNode->NextNode = Temp->NextNode;
+
+		if (Remove->NextNode != NULL)
+			Remove->NextNode->PrevNode = Temp->PrevNode;
+	}
+
+	Remove->PrevNode = NULL;
+	Remove->NextNode = NULL;
+}
+
 int main() {
 	Node* List = NULL;
 
