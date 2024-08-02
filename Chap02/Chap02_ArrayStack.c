0063dee3cb00474f5a8d68f3a745cc2e15369205
진영 조 <jjojin97@gmail.com>
2024-08-02 21:12:27 +0900

    헤더 파일 정의

---
+++ Chap02/Chap02_ArrayStack.c
@@ -0,0 +1,33 @@
+#include "Chap02_ArrayStack.h"
+
+void AS_CreateStack(ArrayStack** Stack, int Capacity)
+{
+}
+
+void AS_DestroyStack(ArrayStack* Stack)
+{
+}
+
+void AS_Push(ArrayStack* Stack, ElementType Data)
+{
+}
+
+ElementType AS_Pop(ArrayStack* Stack)
+{
+	return ElementType();
+}
+
+ElementType As_Top(ArrayStack* Stack)
+{
+	return ElementType();
+}
+
+int AS_GetSize(ArrayStack* Stack)
+{
+	return 0;
+}
+
+int AS_IsEmpty(ArrayStack* Stack)
+{
+	return 0;
+}
--- Chap02/Chap02_ArrayStack.h
+++ Chap02/Chap02_ArrayStack.h
@@ -17,7 +17,12 @@ typedef struct tagArrayStack
 	Node* Nodes;
 } ArrayStack;
 
-void AS_CreateStack(ArrayStack** Stack, int Capacity);
-void AS_DestroyStack(ArrayStack* Stack);
+void		AS_CreateStack(ArrayStack** Stack, int Capacity);
+void		AS_DestroyStack(ArrayStack* Stack);
+void		AS_Push(ArrayStack* Stack, ElementType Data);
+ElementType AS_Pop(ArrayStack* Stack);
+ElementType As_Top(ArrayStack* Stack);
+int			AS_GetSize(ArrayStack* Stack);
+int			AS_IsEmpty(ArrayStack* Stack);
 
 #endif
